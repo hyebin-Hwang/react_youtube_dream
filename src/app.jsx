d221b9b0c81a2cf614a326f93a1e4d64d3d58d2a
(hyebin) <(sksp4334@naver.com)>
2020-10-08 02:03:34 +0900

    add to search

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import SearchHeader from "./component/search_header/search_header";
+import VideoDetail from "./component/video_detail/video_detail";
 import VideoList from "./component/video_list/video_list";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
   const search = (query) => {
     youtube
       .search(query) //
       .then((videos) => setVideos(videos));
+    setSelectedVideo(null);
   };
 
   useEffect(() => {
@@ -20,7 +26,21 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+          ;
+        </div>
+      </section>
     </div>
   );
 }
